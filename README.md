2f13a9656e8550179fa71f0ea91d0070f5c0edd3
lzfsrobot <lzfsmail@gmail.com>
2021-08-10 19:41:09 +0800

    20221/8/10 19:41

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # 1. 前言
 用于快速开发神经网络文件框架，目前只适用于Windows系统
 # 2. 安装
-先要安装[git](https://git-scm.com/)
 命令行执行`pip install nnff`
 # 3. 使用
 ```bash
--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 #############################################
 # File Name: setup.py
-# Author: LiangjunFeng
-# Mail: zhumavip@163.com
-# Created Time:  2018-4-16 19:17:34
+# Author: Liu Zhifei
+# Mail: lzfsmail@163.com
+# Created Time:  2021/8/10 19:24
 #############################################
 
 from setuptools import setup, find_packages            #这个包没有的可以pip一下
 
 setup(
     name = "nnff",      #这里是pip项目发布的名称
-    version = "0.1",  #版本号，数值大的会优先被pip
+    version = "0.2.1",  #版本号，数值大的会优先被pip
     keywords = ["pip", "nnff","framework"],
     description = "An neutral network file framwork.",
     long_description = "An neutral network file framwork.",
@@ -22,8 +22,8 @@ setup(
     author_email = "lzfsmail@gmail.com",
     packages = find_packages(),
     include_package_data = True,
-    entry_points={'console_scripts': ['nnff=nnff.nnff:main']},
+    entry_points={'console_scripts': ['nnff=nnff.test:main']},
     platforms = "win",
-    install_requires = [""]          #这个项目需要的第三方库
+    install_requires = ["gitpython"]          #这个项目需要的第三方库
 )
 
